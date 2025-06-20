f77c0a19be937d1eb01bc289b7d8fda70fe0d3f1
Matt-Harvill <m.harvill23@gmail.com>
2025-06-19 22:39:27 -0700

    removed logger.debug and it passed in 0.88s

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -1,5 +1,4 @@
 from cs336_basics.pretokenization_example import pre_tokenize_dataset_bpe
-from cs336_basics.sorted_frequency_tracker import SortedBytePairFrequencyTracker
 import logging
 import os
 
@@ -9,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 def tokenize_dataset_bpe(
-    input_path: str | os.PathLike, vocab_size: int, special_tokens: list[str], do_fast: bool = True
+    input_path: str | os.PathLike, vocab_size: int, special_tokens: list[str]
 ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
     """
     Tokenizes the dataset using Byte-Pair Encoding (BPE)
@@ -53,45 +52,6 @@ def tokenize_dataset_bpe(
     for i in range(256):
         vocab[vocab_index] = bytes([i])
         vocab_index += 1
-
-    # Use a SortedDict to get_most_freq_pair quicker
-    sorted_byte_pair_frequency_tracker = SortedBytePairFrequencyTracker(bytes_pair_counts=bytes_pair_counts)
-
-    def get_most_freq_pair_fast() -> tuple[bytes, bytes]:
-        return sorted_byte_pair_frequency_tracker.get_most_frequent_bytes_pair()
-
-    # def merge_pair_fast(most_freq_pair: tuple[bytes, bytes]):
-
-    #     # We want to replace all instances of the most frequent pair with a single bytes object
-    #     # We need to handle cases where this byte pair occurs multiple times in the same bytes tuple
-    #     # We will make a list of indices where the most frequent pair occurs
-    #     # We will then replace them iteratively and add/subtract the counts from the sorted byte pair frequency tracker
-    #     # Then since each pair takes two indices, we will keep a counter of how many indices we have processed to update
-    #     # the later pair indices for proper updating
-    #     # We will also need to update the bytes_counts dict
-    #     # Okay let's do this
-
-    #     # Get the indices of the most frequent pair
-    #     for bytes_tuple, count in bytes_counts.items():
-    #         indices_to_replace: list[int] = []
-    #         for i in range(len(bytes_tuple) - 1):
-    #             if bytes_tuple[i] == most_freq_pair[0] and bytes_tuple[i + 1] == most_freq_pair[1]:
-    #                 indices_to_replace.append(i)
-
-    #         if len(indices_to_replace) == 0:
-    #             continue
-
-    #         # Replace the indices
-    #         pairs_replaced: int = 0
-    #         for i in range(len(indices_to_replace) - 1):
-    #             bytes_tuple[indices_to_replace[i]] = bytes_tuple[indices_to_replace[i]] + bytes_tuple[indices_to_replace[i + 1]]
-    #             bytes_tuple[indices_to_replace[i + 1]] = None
-
-    #         # Remove the None values
-
-    #     sorted_byte_pair_frequency_tracker.subtract(byte_pair=most_freq_pair, count=1)
-    #     new_bytes_tuple = most_freq_pair[0] + most_freq_pair[1]
-    #     sorted_byte_pair_frequency_tracker.add(byte_pair=new_bytes_tuple, count=1)
 
     # Helper function for merging to find the most frequent pair this iteration
     def get_most_freq_pair() -> tuple[bytes, bytes]:
@@ -155,24 +115,14 @@ def tokenize_dataset_bpe(
 
             # Subtract before_merge counts
             for pair, pair_count in before_merge_bytes_pair_counts.items():
-                if do_fast:
-                    sorted_byte_pair_frequency_tracker.subtract(
-                        bytes_pair=pair, count=pair_count, bytes_pair_counts=bytes_pair_counts
-                    )
-                else:
-                    bytes_pair_counts[pair] = bytes_pair_counts.get(pair, 0) - pair_count
-                    # Remove zero or negative counts
-                    if bytes_pair_counts[pair] <= 0:
-                        del bytes_pair_counts[pair]
+                bytes_pair_counts[pair] = bytes_pair_counts.get(pair, 0) - pair_count
+                # Remove zero or negative counts
+                if bytes_pair_counts[pair] <= 0:
+                    del bytes_pair_counts[pair]
 
             # Add after_merge counts
             for pair, pair_count in after_merge_bytes_pair_counts.items():
-                if do_fast:
-                    sorted_byte_pair_frequency_tracker.add(
-                        bytes_pair=pair, count=pair_count, bytes_pair_counts=bytes_pair_counts
-                    )
-                else:
-                    bytes_pair_counts[pair] = bytes_pair_counts.get(pair, 0) + pair_count
+                bytes_pair_counts[pair] = bytes_pair_counts.get(pair, 0) + pair_count
 
             # Store for later processing
             bytes_tuples_to_delete.append(bytes_tuple)
@@ -190,12 +140,8 @@ def tokenize_dataset_bpe(
 
     # Add all other tokens until we've filled the vocabulary
     while vocab_index < vocab_size and len(bytes_pair_counts) > 0:
-        logger.debug(f"bytes_counts: {bytes_counts}")
-        logger.debug(f"byte_pair_counts: {bytes_pair_counts}")
-
         # Find the most frequent byte pair
-        most_freq_pair = get_most_freq_pair_fast() if do_fast else get_most_freq_pair()
-        logger.debug(f"most_freq_pair: {most_freq_pair}")
+        most_freq_pair = get_most_freq_pair()
 
         # Add to vocabulary
         merged_bytes = most_freq_pair[0] + most_freq_pair[1]
@@ -205,34 +151,10 @@ def tokenize_dataset_bpe(
         # Merge it
         merge_pair(most_freq_pair)
 
-    logger.debug(f"Final bytes_counts: {bytes_counts}")
-    logger.debug(f"Final byte_pair_counts: {bytes_pair_counts}")
-
     return (vocab, merges)
 
 
 if __name__ == "__main__":
-    # # Test the tokenize function
-    # import cProfile
-    # import pstats
-
-    # # Profile and save results
-    # cProfile.run(
-    #     'tokenize_dataset_bpe(input_path="/home/matthew/Code/assignment1-basics/data/TinyStoriesV2-GPT4-train.txt", vocab_size=0, special_tokens=["<|endoftext|>"])',
-    #     "profile_stats",
-    # )
-
-    # # Profile and save results
-    # cProfile.run(
-    #     'tokenize_dataset_bpe(input_path="/home/matthew/Code/assignment1-basics/data/simple_test.txt", vocab_size=10, special_tokens=["<|endoftext|>"])',
-    #     "profile_stats",
-    # )
-
-    # # Analyze results
-    # stats = pstats.Stats("profile_stats")
-    # stats.sort_stats("cumulative")
-    # stats.print_stats(10)  # Top 10 functions
-
     vocab, merges = tokenize_dataset_bpe(
         input_path="/home/matthew/Code/assignment1-basics/data/test.txt",
         vocab_size=1000,
