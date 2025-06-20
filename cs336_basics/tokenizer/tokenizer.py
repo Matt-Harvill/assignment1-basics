6be6f390334188f9ee29fdc520e178a9f4704299
Matt-Harvill <m.harvill23@gmail.com>
2025-06-20 15:00:12 -0700

    tokenizer.encode appears to work

--- cs336_basics/tokenizer/tokenizer.py
+++ cs336_basics/tokenizer/tokenizer.py
@@ -1,6 +1,8 @@
 from collections.abc import Iterable, Iterator
 
+from cs336_basics.utils.paths import get_artifacts_path
 from .utils import load_vocab_and_merges, convert_vocab_to_bytes, convert_merges_to_bytes
+from .pretokenizer import pre_tokenize_chunk_ordered
 from pathlib import Path
 
 
@@ -19,6 +21,7 @@ class Tokenizer:
         Construct a tokenizer from a given vocabulary, list of merges, and (optionally) a list of special tokens.
         """
         self.vocab = vocab
+        self.inverse_vocab = {v: k for k, v in vocab.items()}
         self.merges = merges
         self.special_tokens = special_tokens
 
@@ -42,12 +45,54 @@ class Tokenizer:
         Encode an input text into a sequence of token IDs.
         """
 
-        # text_segments, special_tokens, special_tokens_first = pre_tokenize_chunk_ordered(text, [] if self.special_tokens is None else self.special_tokens)
+        # First we pre-tokenize the text (saving positional information about the special tokens)
+        text_segments_bytes, special_tokens, special_tokens_first = pre_tokenize_chunk_ordered(
+            text, [] if self.special_tokens is None else self.special_tokens
+        )
 
-        # for text_segment in text_segments:
-        #     if text_segment in self.vocab:
+        # Then we loop over the merges and apply them to our text segments
+        for merge in self.merges:
+            for text_segment_subset_bytes in text_segments_bytes:
+                for text_segment_bytes in text_segment_subset_bytes:
+                    merged_segment_bytes: list[bytes] = []
+                    i = 0
+                    while i < len(text_segment_bytes) - 1:
+                        if text_segment_bytes[i] == merge[0] and text_segment_bytes[i + 1] == merge[1]:
+                            # Replace two bytes objects with the concatenated bytes object
+                            merged_segment_bytes.append(text_segment_bytes[i] + text_segment_bytes[i + 1])
+                            i += 2
+                        else:
+                            merged_segment_bytes.append(text_segment_bytes[i])
+                            i += 1
+                    text_segment_bytes = tuple(merged_segment_bytes)
 
-        raise NotImplementedError("encode method not yet implemented")
+        # After we've merged all the bytes, we need to convert the bytes objects to token ids
+        token_ids: list[list[int]] = []
+        for text_segment_subset_bytes in text_segments_bytes:
+            token_ids_subset: list[int] = []
+            for text_segment_bytes in text_segment_subset_bytes:
+                for bytes_object in text_segment_bytes:
+                    # bytes_object will always be in the vocab
+                    token_ids_subset.append(self.inverse_vocab[bytes_object])
+            token_ids.append(token_ids_subset)
+
+        # Convert special tokens to token ids
+        special_tokens_ids = [self.inverse_vocab[special_token.encode("utf-8")] for special_token in special_tokens]
+
+        # Interleave text segments with special tokens
+        all_token_ids: list[int] = []
+        for i in range(min(len(token_ids), len(special_tokens_ids))):
+            if special_tokens_first:
+                all_token_ids.append(special_tokens_ids[i])
+            all_token_ids.extend(token_ids[i])
+            if not special_tokens_first:
+                all_token_ids.append(special_tokens_ids[i])
+        if len(token_ids) > len(special_tokens_ids):
+            all_token_ids.extend(token_ids[-1])
+        if len(token_ids) < len(special_tokens_ids):
+            all_token_ids.append(special_tokens_ids[-1])
+
+        return all_token_ids
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
         """
@@ -63,3 +108,39 @@ class Tokenizer:
         Decode a sequence of token IDs into text
         """
         raise NotImplementedError("decode method not yet implemented")
+
+
+# Test the tokenizer on a few edge cases (small examples)
+if __name__ == "__main__":
+    # Use argparse to get the vocab and merges files from tokenizer name (and some utils to get the path)
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--tokenizer_name", type=str, default="TinyStoriesV2-GPT4-train")
+    args = parser.parse_args()
+    artifacts_path = get_artifacts_path()
+    vocab_filepath = artifacts_path / "tokenizers" / args.tokenizer_name / "vocab.json"
+    merges_filepath = artifacts_path / "tokenizers" / args.tokenizer_name / "merges.txt"
+    tokenizer = Tokenizer.from_files(vocab_filepath, merges_filepath, special_tokens=["<|endoftext|>"])
+
+    # Test the tokenizer on a few small examples
+    print(tokenizer.encode("Hello, world!"))
+    print(tokenizer.encode("Hello, world! This is a test."))
+    print(tokenizer.encode("Hello, world! This is a test. This is a test. This is a test. This is a test."))
+
+    # Test the tokenizer on a few edge cases (special tokens)
+    print(
+        tokenizer.encode(
+            "Hello, world! <|endoftext|><|endoftext|> this is a simple example<|endoftext|> <|endoftext|> of a sentence"
+        )
+    )
+    print(
+        tokenizer.encode(
+            "<|endoftext|>Hello, world! <|endoftext|> this is a simple example <|endoftext|> of a sentence<|endoftext|>"
+        )
+    )
+    print(
+        tokenizer.encode(
+            " <|endoftext|>Hello, world! <|endoftext|> this is a simple example <|endoftext|> of a sentence<|endoftext|><|endoftext|><|endoftext|>"
+        )
+    )
